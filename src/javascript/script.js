7dfcec2209db54debb6b22b2c55ef714e6d0efe4
Dedi Indrawan <dediindrawan812@gmail.com>
2023-06-30 14:32:59 +0700

    first commit

---
+++ src/javascript/script.js
@@ -0,0 +1,5 @@
+const btnOpenInvitation = document.querySelector('.btn-open-invitation');
+
+btnOpenInvitation.addEventListener('click', () => {
+    document.querySelector('.hero').style.display = 'none';
+});
