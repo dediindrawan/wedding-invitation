ed7ac9f1e5e9551909c9951dc25f0f028c6c29ab
Dedi Indrawan <dediindrawan812@gmail.com>
2023-06-30 15:24:28 +0700

    first commit

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,8 @@
 const btnOpenInvitation = document.querySelector('.btn-open-invitation');
-
+const navlink = document.querySelector('.navlink');
+const container = document.querySelector('.container');
 btnOpenInvitation.addEventListener('click', () => {
-    document.querySelector('.hero').style.display = 'none';
+    document.querySelector('.hero-image').classList.add('close-page');
+    navlink.classList.remove('hide-content');
+    container.classList.remove('hide-content');
 });
